1b9e7943acbe2f8ed694d7b72e924766a941cd76
Dan Munro <dan@danmunro.com>
2022-01-14 21:44:03 -0800

    fix env

--- internal/kafka/reader.go
+++ internal/kafka/reader.go
@@ -9,7 +9,7 @@ func GetReader() *kafka.Consumer {
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
 		"security.protocol": os.Getenv("KAFKA_SECURITY_PROTOCOL"),
-		"sasl.mechanisms": os.Getenv("KAFKA_SASL_MECHANISMS"),
+		"sasl.mechanisms": os.Getenv("KAFKA_SASL_MECHANISM"),
 		"sasl.username": os.Getenv("KAFKA_SASL_USERNAME"),
 		"sasl.password": os.Getenv("KAFKA_SASL_PASSWORD"),
 		"group.id": "otto",
--- internal/kafka/writer.go
+++ internal/kafka/writer.go
@@ -10,7 +10,7 @@ func CreateWriter() *kafka.Producer {
 	producer, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
 		"security.protocol": os.Getenv("KAFKA_SECURITY_PROTOCOL"),
-		"sasl.mechanisms": os.Getenv("KAFKA_SASL_MECHANISMS"),
+		"sasl.mechanisms": os.Getenv("KAFKA_SASL_MECHANISM"),
 		"sasl.username": os.Getenv("KAFKA_SASL_USERNAME"),
 		"sasl.password": os.Getenv("KAFKA_SASL_PASSWORD"),
 	})
