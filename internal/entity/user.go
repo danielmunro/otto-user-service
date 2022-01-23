da52a4d47ffeefc4e169e8698099257b2a07e5c9
Dan Munro <dan@danmunro.com>
2022-01-23 15:22:25 -0800

    format and username

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -10,36 +10,36 @@ import (
 
 type User struct {
 	gorm.Model
-	Uuid uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
-	CognitoId uuid.UUID
-	SRP string
+	Uuid             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+	CognitoId        uuid.UUID
+	SRP              string
 	LastSessionToken string
-	LastAccessToken string
-	LastIdToken string
+	LastAccessToken  string
+	LastIdToken      string
 	LastRefreshToken string
-	DeviceGroupKey string
-	DeviceKey string
-	Name    string
-	Username string `gorm:"not null"`
-	ProfilePic string
-	BioMessage string
-	AddressStreet string
-	AddressCity string
-	AddressZip string
-	CurrentEmail string `gorm:"unique;not null"`
-	CurrentPassword string `gorm:"not null"`
-	Birthday string
-	Verified bool `gorm:"not null"`
-	Emails []*Email
-	Passwords []*Password
+	DeviceGroupKey   string
+	DeviceKey        string
+	Name             string
+	Username         string `gorm:"unique;not null"`
+	ProfilePic       string
+	BioMessage       string
+	AddressStreet    string
+	AddressCity      string
+	AddressZip       string
+	CurrentEmail     string `gorm:"unique;not null"`
+	CurrentPassword  string `gorm:"not null"`
+	Birthday         string
+	Verified         bool `gorm:"not null"`
+	Emails           []*Email
+	Passwords        []*Password
 }
 
 func CreateUser(newUser *model.NewUser, cognitoId string) *User {
 	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
 	return &User{
-		CurrentEmail: newUser.Email,
+		CurrentEmail:    newUser.Email,
 		CurrentPassword: string(hashedPassword),
-		CognitoId: uuid.MustParse(cognitoId),
+		CognitoId:       uuid.MustParse(cognitoId),
 		Emails: []*Email{
 			CreateEmail(newUser.Email),
 		},
