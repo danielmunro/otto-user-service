7d81b8a4b68e19e213ef30e88f186330af60ae7f
Dan Munro <dan@danmunro.com>
2022-01-26 21:08:51 -0800

    add logging

--- internal/kafka/consumer.go
+++ internal/kafka/consumer.go
@@ -9,7 +9,6 @@ import (
 )
 
 func InitializeAndRunLoop() {
-
 	userRepository := repository.CreateUserRepository(db.CreateDefaultConnection())
 	err := loopKafkaReader(userRepository)
 	if err != nil {
--- internal/kafka/reader.go
+++ internal/kafka/reader.go
@@ -1,18 +1,30 @@
 package kafka
 
 import (
+	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
 	"os"
 )
 
 func GetReader() *kafka.Consumer {
+	log.Print(
+		fmt.Printf(
+			"debug kafka reader :: %s %s %s %s %s",
+			os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
+			os.Getenv("KAFKA_SECURITY_PROTOCOL"),
+			os.Getenv("KAFKA_SASL_MECHANISMS"),
+			os.Getenv("KAFKA_SASL_USERNAME"),
+			os.Getenv("KAFKA_SASL_PASSWORD"),
+		),
+	)
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": os.Getenv("KAFKA_BOOTSTRAP_SERVERS"),
 		"security.protocol": os.Getenv("KAFKA_SECURITY_PROTOCOL"),
-		"sasl.mechanisms": os.Getenv("KAFKA_SASL_MECHANISM"),
-		"sasl.username": os.Getenv("KAFKA_SASL_USERNAME"),
-		"sasl.password": os.Getenv("KAFKA_SASL_PASSWORD"),
-		"group.id": "otto",
+		"sasl.mechanisms":   os.Getenv("KAFKA_SASL_MECHANISM"),
+		"sasl.username":     os.Getenv("KAFKA_SASL_USERNAME"),
+		"sasl.password":     os.Getenv("KAFKA_SASL_PASSWORD"),
+		"group.id":          "otto",
 		"auto.offset.reset": "earliest",
 	})
 
